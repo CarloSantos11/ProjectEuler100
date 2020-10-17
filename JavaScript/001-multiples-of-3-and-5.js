1a637603db5b35251ab5ccfdd02ae9982f879cc2
YafiWebDev <yafiwebdev@gmail.com>
2020-10-17 09:12:02 +0200

    Solve problems 001 and 002 in JS and PHP

---
+++ JavaScript/001-multiples-of-3-and-5.js
@@ -0,0 +1,25 @@
+/* 
+	If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+	Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+*/
+
+function multiplesOf3and5(number) {
+	let sum = 0;
+
+	for (let num = 1; num < number; num++) {
+
+		if (
+			num % 3 === 0 ||
+			num % 5 === 0
+		) {
+			sum += num;
+		}
+
+	}
+
+	return sum;
+}
+
+multiplesOf3and5(1000);
+
