be4d03c111079fbed29e1a2afcf79613d5789bd0
YafiWebDev <yafiwebdev@gmail.com>
2020-10-22 15:32:36 +0200

    Add problem description

--- PHP/006-sum-square-difference.php
+++ PHP/006-sum-square-difference.php
@@ -1,5 +1,17 @@
 <?php
 
+/* 
+	The sum of the squares of the first ten natural numbers is,
+	1² + 2² + ... + 10² = 385
+
+	The square of the sum of the first ten natural numbers is,
+
+	(1 + 2 + ... + 10)² = 552 = 3025
+	Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+
+	Find the difference between the sum of the squares of the first n natural numbers and the square of the sum.
+*/
+
 function sumSquareDifference($number) {
 
 	$sumOfSquares = $sum = 0;
