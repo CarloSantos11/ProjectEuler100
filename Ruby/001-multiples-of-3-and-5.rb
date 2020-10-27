1eeeb2654c872eccbb2e175af4dc99880efdd665
Carlo Santos <carlo@snapit.solutions>
2020-10-27 17:54:37 -0500

    starting ruby directory TODO: add comments

---
+++ Ruby/001-multiples-of-3-and-5.rb
@@ -0,0 +1,23 @@
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+# The sum of these multiples is 23.
+
+# Find the sum of all the multiples of 3 or 5 below the provided parameter value number.
+
+
+def multiplesOf3and5(number) 
+	sum = 0
+	num = 1
+
+	while num < number do 
+
+		if num % 3 === 0 || num % 5 === 0
+			sum += num;
+		end
+		num += 1
+	end
+
+	return sum
+end
+
+puts multiplesOf3and5(1000);
+
