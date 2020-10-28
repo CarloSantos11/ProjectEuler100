8b30ec01d8472a157651478bda5660514b68ffff
Ares358 <52101053+Ares358@users.noreply.github.com>
2020-10-28 21:01:32 +0530

    Update 001-multiples-of-3-and-5.java
    Added parameter n for the function multiples() to serve as limit

--- Java/001-multiples-of-3-and-5.java
+++ Java/001-multiples-of-3-and-5.java
@@ -1,10 +1,10 @@
 class Solution
 {
-  public static long multiples()
+  public static long multiples(long n)
   {
     long sum = 0 ;
       
-      for(long i = 1 ; i < 1000 ; i++) 
+      for(long i = 1 ; i < n ; i++) 
     {
     	if((i % 3 == 0 || i % 5 == 0))
         sum+=i;
@@ -14,7 +14,7 @@ class Solution
   }
 	public static void main(String[] args) 
   {
-      long ans = multiples();
+      long ans = multiples(1000);
       System.out.println(ans);
   }
 }
