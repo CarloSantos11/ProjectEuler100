f88069912e8a357e5afe0ac1a4648def3e708fac
tharindu <tharindu@huubap.com>
2020-10-29 10:48:13 +0800

    005-smallest-multiple.rb #7 - update based on the review comments

--- Ruby/005-smallest-multiple.rb
+++ Ruby/005-smallest-multiple.rb
@@ -2,23 +2,35 @@
 
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
-def smallestMultiple
-  lcm = 1 # least common multiple
-  (2..20).each do |i|
-    lcm *= i / greatestCommonDivisor(lcm, i)
+def smallestMultiple(maxFactor) # maxFactor - max positive factor that use to check the divisible from 1 to maxFactor
+  lcm = 1 # least common multiple - the smallest positive integer that is divisible by two integers
+  (2..maxFactor).each do |factor| # Iterate from 2 to maxFactor. No need to consider 1 here since it's a factor of all integers.
+    lcm *= factor / greatestCommonDivisor(lcm, factor)
   end
 
   lcm
 end
 
-def greatestCommonDivisor(a, b)
-  while b > 0
-    a %= b
-    return b if a == 0
-    b %= a
+# Find the largest positive integer that divides each of the integers
+# Ex:
+#   <factor>  -->  <greatestCommonDivisor>
+#       2     -->     1
+#       3     -->     1
+#       4     -->     2
+#       5     -->     1
+#       6     -->     6
+#       7     -->     1
+#       8     -->     4
+#       ...............
+#       ...............
+def greatestCommonDivisor(lcm, factor)
+  while factor > 0
+    lcm %= factor
+    return factor if lcm == 0
+    factor %= lcm
   end
 
-  a
+  lcm
 end
 
-puts smallestMultiple
+puts smallestMultiple(20) # put 20 here to solve problem 5
