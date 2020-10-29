793dc9cbfbf562185b10d3e100bce06e309babbd
MrityunjayaTripathi <58814328+Error404m@users.noreply.github.com>
2020-10-29 06:27:03 -0700

    Solve 003-largest-prime-factor in Go (#72)

---
+++ GO/003-largest-prime-factor.go
@@ -0,0 +1,54 @@
+//Problem 3 | The prime factors of 13195 are 5, 7, 13 and 29.
+
+//What is the largest prime factor of the number 600851475143 ?
+
+
+package main
+
+import "fmt"
+import "math"
+
+//All primes aside from 2 and 3 are of the form 6k+-1 
+func not_prime(n int64) bool {
+	if (n-1)%6 == 0 {
+		return false
+	}
+	if (n+1)%6 == 0 {
+		return false
+	}
+	return true
+}
+
+func is_prime(n int64) bool {
+	if not_prime(n) {
+		return false
+	}
+	max := int64(math.Ceil(math.Sqrt(float64(n))))
+	for i := int64(2); i < max; i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
+}
+
+//main function
+func main() {
+	n := int64(600851475143)
+	largest := int64(0)
+
+	if (n % 2) == 0 {
+		largest = 2
+	}
+
+	for i := int64(3); i < int64(math.Sqrt(float64(n))); i += 2 {
+		if (n % i) == 0 {
+			if is_prime(i) {
+				fmt.Print(i, "\n")
+				largest = i
+			}
+		}
+	}
+	fmt.Printf("Largest: %d\n", largest)
+
+}
